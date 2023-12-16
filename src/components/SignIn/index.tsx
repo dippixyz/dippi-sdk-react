ec65f2bbcf14eb208c3d47391a03edba9d093606
joseEnrique9797 <jbarrientos97@outlook.com>
2023-12-15 19:26:09 -0600

    [ADD] template structure components

---
+++ src/components/SignIn/index.tsx
@@ -0,0 +1,7 @@
+var styles = {"test":"_3ybTi"};
+var ExampleComponent = function ExampleComponent(_ref:any) {
+    var text = _ref.text;
+    return React.createElement("div", {
+      className: styles.test
+    }, "Example Component: ", text);
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+var React = require('react');
+import ExampleComponent from './components/SignIn';
+exports.ExampleComponent = ExampleComponent;
